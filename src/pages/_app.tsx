bcf4a6b18e187108ad2eccbb0132b21c3a2eafda
Rohmad Eko Wahyudi <rohmad.ew@gmail.com>
2024-05-12 19:25:20 +0700

    install daisy ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -10,8 +11,40 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
+const AppHead = () => {
+  return (
+    <Head>
+      <meta name="google" content="notranslate" />
+      <link rel="shortcut icon" href="/favicon/favicon.svg" />
+      <link rel="icon" href="/favicon/favicon.svg" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicon/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon/favicon-16x16.png"
+      />
+    </Head>
+  );
+};
+
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return (
+    <>
+      <AppHead />
+      {getLayout(<Component {...pageProps} />)}
+    </>
+  );
 }
