44e99658c4d45f7bcd5d96e59e20689681038e7d
Rohmad Eko Wahyudi <rohmad.ew@gmail.com>
2024-05-18 07:02:42 +0700

    load more

--- src/components/laporan/LaporanList.tsx
+++ src/components/laporan/LaporanList.tsx
@@ -3,6 +3,7 @@ import LaporanListDetail from "@/components/laporan/LaporanListDetail";
 import LaporanTidakAda from "@/components/laporan/LaporanTidakAda";
 import { useListUserReport } from "@/hooks/user_report/useListUserReport";
 import { initialPaginationReportQueryParams } from "@/store/report/PaginationReportStore";
+import { Report } from "@/types/report/report";
 import * as React from "react";
 
 export interface LaporanListProps {
@@ -14,8 +15,18 @@ const LaporanList: React.FC<LaporanListProps> = (props: LaporanListProps) => {
   const [filter, setFilter] = React.useState<PaginationReportQueryParams>(
     initialPaginationReportQueryParams
   );
-  const { data: listUserReport, isPending: isLoading } =
-    useListUserReport(filter);
+  const [listUserReport, setListUserReport] = React.useState<Report[]>([]);
+  const [isLastUserReport, setIsLastUserReport] =
+    React.useState<boolean>(false);
+
+  const { data, isPending: isLoading } = useListUserReport(filter);
+
+  React.useEffect(() => {
+    if (!isLoading && !!data) {
+      setListUserReport((prev) => [...prev, ...data]);
+      setIsLastUserReport(data.length < filter.limit);
+    }
+  }, [isLoading, data]);
 
   return (
     <>
@@ -38,6 +49,21 @@ const LaporanList: React.FC<LaporanListProps> = (props: LaporanListProps) => {
           {listUserReport?.map((report) => (
             <LaporanListDetail report={report} key={report.xid} />
           ))}
+          {!isLoading && !isLastUserReport && (
+            <div className="flex justify-center p-5">
+              <button
+                onClick={() => {
+                  setFilter((prev) => ({
+                    ...prev,
+                    skip: prev.skip + prev.limit,
+                  }));
+                }}
+                className="btn bg-primary btn-sm text-white"
+              >
+                Load More
+              </button>
+            </div>
+          )}
           {!isLoading && listUserReport?.length === 0 && <LaporanTidakAda />}
           {isLoading && (
             <div className="flex justify-center p-5">
