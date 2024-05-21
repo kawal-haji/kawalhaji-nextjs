32a5ae2c5dfce2d1dfdafdb70807126b10feac70
Rohmad Eko Wahyudi <rohmad.ew@gmail.com>
2024-05-22 06:57:11 +0700

    pause video when close

--- src/components/image/ImagePreview.tsx
+++ src/components/image/ImagePreview.tsx
@@ -9,6 +9,12 @@ export interface ImagePreviewProps {
 const ImagePreview: React.FC<ImagePreviewProps> = ({ attachment }) => {
   const imageModal = React.useRef<HTMLDialogElement>(null);
   const videoModal = React.useRef<HTMLDialogElement>(null);
+  const videoRef = React.useRef<HTMLVideoElement>(null);
+
+  const handleCloseVideoModal = () => {
+    videoModal.current?.close();
+    videoRef.current?.pause();
+  };
 
   return (
     <>
@@ -25,11 +31,11 @@ const ImagePreview: React.FC<ImagePreviewProps> = ({ attachment }) => {
           onClick={() => videoModal.current?.showModal()}
         >
           <img
-            src={attachment.file.url}
+            src="/icons/background_video.svg"
             alt="like"
             className="w-full rounded-md object-cover"
           />
-          <div className="absolute inset-0 bg-black bg-opacity-35 flex items-center justify-center  rounded-md">
+          <div className="absolute inset-0 bg-black bg-opacity-30 flex items-center justify-center  rounded-md">
             <img src="/icons/play.svg" alt="Play" />
           </div>
         </div>
@@ -56,24 +62,23 @@ const ImagePreview: React.FC<ImagePreviewProps> = ({ attachment }) => {
         </div>
       </dialog>
       <dialog ref={videoModal} className="modal">
-        <div
-          className="modal-backdrop"
-          onClick={() => videoModal.current?.close()}
-        >
+        <div className="modal-backdrop" onClick={handleCloseVideoModal}>
           <button>close</button>
         </div>
         <div className="modal-box p-0">
           <button
             className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-            onClick={() => videoModal.current?.close()}
+            onClick={handleCloseVideoModal}
           >
             ✕
           </button>
           <video
-            src={attachment.file.url}
+            ref={videoRef}
             className="w-full rounded-md object-cover"
             controls
-          />
+          >
+            <source src={attachment.file.url} type="video/mp4" />
+          </video>
         </div>
       </dialog>
     </>
